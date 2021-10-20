d901121a6b309056e8f30ba8b7781f9d0fd6f74c
alex <alexangel.j@gmail.com>
2021-10-19 17:38:19 -0700

    fix(Pool): updates Pool class with new set/get for VirtualPool and pool state

--- test/Pool.spec.ts
+++ test/Pool.spec.ts
@@ -1,6 +1,6 @@
 import { Token } from '@uniswap/sdk-core'
 import { VirtualPool } from '../src/entities'
-import { Calibration } from '../src'
+import { Calibration, Pool } from '../src'
 import { parseWei, Time } from 'web3-units'
 import { callDelta, callPremium } from '@primitivefinance/v2-math'
 import { AddressZero } from '@ethersproject/constants'
@@ -14,6 +14,7 @@ describe('Test pool', function() {
     pool = new VirtualPool(calibration, parseWei(1 - callDelta(10, 1, 1, 10)), parseWei(1))
     prices = [10, 1]
   })
+
   it('gets the theoretical liquidity value', async function() {
     const theoretical = pool.getTheoreticalLiquidityValue()
     expect(theoretical).toBeCloseTo(10 - callPremium(10, 1, 1, 10))
@@ -27,5 +28,19 @@ describe('Test pool', function() {
   it('gets the theoretical max fee', async function() {
     const max = pool.getTheoreticalMaxFee(1)
     expect(max).toBeCloseTo(callPremium(10, 1, 1, 10))
+  })
+
+  it('new Pool(...).poolId', async function() {
+    const main = new Pool({
+      factory: AddressZero,
+      risky: new Token(1, AddressZero, 18),
+      stable: new Token(1, AddressZero, 18),
+      strike: 10,
+      sigma: 1,
+      maturity: 10,
+      lastTimestamp: 1
+    })
+
+    expect(main).toBeDefined
   })
 })
