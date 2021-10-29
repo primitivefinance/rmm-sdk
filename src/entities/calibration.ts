4cd13f93f8c1447145db3686b4cbb399b75e7ba6
alex <alexangel.j@gmail.com>
2021-10-29 16:25:08 -0700

    feat(calibration-gamma): adds a gamma parameter to calibration and adds a test for it

--- src/entities/calibration.ts
+++ src/entities/calibration.ts
@@ -20,33 +20,50 @@ export class Calibration extends Engine {
    * @notice Time class with a raw value in seconds
    */
   public readonly maturity: Time
+  /**
+   * @notice Gamma, equal to 1 - fee %, as a Percentage instance with 4 precision
+   */
+  public readonly gamma: Percentage
 
   /**
    *
    * @param strike Strike price as a float with precision equal to stable token decimals
    * @param sigma Volatility percentage as a float, e.g. 1 = 100%
    * @param maturity Timestamp in seconds
+   * @param gamma 1 - fee, as a float
    */
-  constructor(factory: string, risky: Token, stable: Token, strike: number, sigma: number, maturity: number) {
+  constructor(
+    factory: string,
+    risky: Token,
+    stable: Token,
+    strike: number,
+    sigma: number,
+    maturity: number,
+    gamma: number
+  ) {
     super(factory, risky, stable)
     this.strike = parseWei(strike, stable.decimals)
     this.sigma = parsePercentage(sigma)
     this.maturity = new Time(maturity) // in seconds, because `block.timestamp` is in seconds
+    this.gamma = parsePercentage(gamma)
   }
 
   /**
    * @notice Keccak256 hash of the calibration parameters and the engine address
    */
   get poolId(): string {
-    return Calibration.computePoolId(this.address, this.strike.raw, this.sigma.raw, this.maturity.raw)
+    return Calibration.computePoolId(this.address, this.strike.raw, this.sigma.raw, this.maturity.raw, this.gamma.raw)
   }
 
   public static computePoolId(
     engine: string,
     strike: string | BigNumber,
     sigma: string | BigNumber,
-    maturity: string | number
+    maturity: string | number,
+    gamma: string | BigNumber
   ): string {
-    return keccak256(solidityPack(['address', 'uint128', 'uint32', 'uint32'], [engine, strike, sigma, maturity]))
+    return keccak256(
+      solidityPack(['address', 'uint128', 'uint32', 'uint32', 'uint32'], [engine, strike, sigma, maturity, gamma])
+    )
   }
 }
--- test/Calibration.spec.ts
+++ test/Calibration.spec.ts
@@ -6,13 +6,13 @@ import { Engine } from '../src/entities/engine'
 import { Calibration } from '../src/entities/calibration'
 
 describe('Calibration', function() {
-  let cal: Calibration, token0: Token, token1: Token, strike: number, sigma: number, maturity: number
+  let cal: Calibration, token0: Token, token1: Token, strike: number, sigma: number, maturity: number, gamma: number
 
   beforeEach(async function() {
     token0 = new Token(1, AddressZero, 18)
     token1 = new Token(1, AddressZero, 18)
-    ;[strike, sigma, maturity] = [10, 1, Time.YearInSeconds]
-    cal = new Calibration(AddressZero, token0, token1, strike, sigma, maturity)
+    ;[strike, sigma, maturity, gamma] = [10, 1, Time.YearInSeconds, 1 - 0.0015]
+    cal = new Calibration(AddressZero, token0, token1, strike, sigma, maturity, gamma)
   })
 
   it('#poolId', async function() {
@@ -20,7 +20,8 @@ describe('Calibration', function() {
       Engine.computeEngineAddress(AddressZero, token0.address, token1.address, Engine.BYTECODE),
       parseWei(strike, token1.decimals).raw,
       parsePercentage(sigma).raw,
-      maturity
+      maturity,
+      parsePercentage(gamma).raw
     )
     expect(cal.poolId).toBe(expected)
   })
@@ -35,5 +36,9 @@ describe('Calibration', function() {
 
   it('#maturity', async function() {
     expect(cal.maturity.raw).toBe(maturity)
+  })
+
+  it('#gamma', async function() {
+    expect(cal.gamma.float).toBe(gamma)
   })
 })
